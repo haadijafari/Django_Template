6d6636a820a3ef474e4839801d146332ca9737cd
Hadi Jafari <haadijafari2003@gmail.com>
2025-12-26 23:16:13 +0330

    [FIX] (core) Fix User Config Change
    [+] Add the new user config due to the change of auths.user to apps.accounts
    [+] Change the core settings formatting

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -1,18 +1,18 @@
 from .base import *
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
+    "127.0.0.1",
+    "localhost",
 ]
 
 INTERNAL_IPS = [
-    '127.0.0.1',
-    'localhost',
+    "127.0.0.1",
+    "localhost",
 ]
 
 INSTALLED_APPS += [
-    'debug_toolbar',
-    'django_extensions',
+    "debug_toolbar",
+    "django_extensions",
 ]
 
 MIDDLEWARE += [
@@ -20,18 +20,20 @@ MIDDLEWARE += [
 ]
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
-STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_ROOT = BASE_DIR / 'media'
+# Root folders for collected static files and media files (via 'collectstatic')
+STATIC_ROOT = BASE_DIR / "static_cdn"
+MEDIA_ROOT = BASE_DIR / "media_cdn"
 
 STATICFILES_DIRS = [
     # BASE_DIR / "apps/frontend/next/src",
+    BASE_DIR / "static",
 ]
 
 # reCaptcha
-SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
+SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
 
 # Cros Origin Settings
 CORS_ALLOWED_ORIGINS = [
--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -1,40 +1,44 @@
 from .base import *
 
 ALLOWED_HOSTS = [
-    'https://example.com',
+    "https://example.com",
 ]
 
 INTERNAL_IPS = [
-    'https://example.com',
+    "https://example.com",
 ]
 
 MIDDLEWARE += [
-    'csp.middleware.CSPMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "csp.middleware.CSPMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # reCaptcha
-RECAPTCHA_PUBLIC_KEY = os.environ['RECAPTCHA_PUBLIC_KEY_V2']
-RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY_V2']
+RECAPTCHA_PUBLIC_KEY = os.environ["RECAPTCHA_PUBLIC_KEY_V2"]
+RECAPTCHA_PRIVATE_KEY = os.environ["RECAPTCHA_PRIVATE_KEY_V2"]
 # RECAPTCHA_DOMAIN = 'www.recaptcha.net'
 RECAPTCHA_REQUIRED_SCORE = 0.75
 
-STATIC_ROOT = '/home/[USER]/public_html/static'
-MEDIA_ROOT = '/home/[USER]/public_html/media'
-# STATIC_ROOT = BASE_DIR / "static_cdn"
-# MEDIA_ROOT = BASE_DIR / "media_cdn"
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# Root folders for collected static files and media files (via 'collectstatic')
+STATIC_ROOT = "/home/[USER]/public_html/static"
+MEDIA_ROOT = "/home/[USER]/public_html/media"
+
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
     # BASE_DIR / "apps/frontend/vite-react/dist",
 ]
 
 # Security
-CSRF_COOKIE_SECURE = True  # to avoid transmitting the CSRF cookie over HTTP accidentally.
-SESSION_COOKIE_SECURE = True  # to avoid transmitting the session cookie over HTTP accidentally.
+CSRF_COOKIE_SECURE = (
+    True  # to avoid transmitting the CSRF cookie over HTTP accidentally.
+)
+SESSION_COOKIE_SECURE = (
+    True  # to avoid transmitting the session cookie over HTTP accidentally.
+)
 
 SECURE_HSTS_SECONDS = 15780000  # 6 Months as Recommended
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
